b883eaf2112e8540d982f3ea0511efff05fee41c
Tal Taub <taltaub22@gmail.com>
2018-05-20 18:10:08 +0300

    more fixes

--- routers/crudRouter.js
+++ routers/crudRouter.js
@@ -6,90 +6,88 @@ const MsLogger = require('@first-lego-league/ms-logger').Logger
 const MONGU_URI = process.env.MONGO
 
 exports.getRouter = function (Model) {
-    const router = express.Router()
+  const router = express.Router()
 
-    router.get('/all', (req, res) => {
-        MongoClient.connect(MONGU_URI).then(connection => {
-            connection.db().collection(Model.collectionName).find().toArray().then(data => {
-                res.send(data)
-            })
-        }).catch(err => {
-            MsLogger.error(err)
-            res.sendStatus(500)
-        })
+  router.get('/all', (req, res) => {
+    MongoClient.connect(MONGU_URI).then(connection => {
+      connection.db().collection(Model.collectionName).find().toArray().then(data => {
+        res.send(data)
+      })
+    }).catch(err => {
+      MsLogger.error(err)
+      res.sendStatus(500)
     })
+  })
 
-    router.get('/:id', (req, res) => {
-        MongoClient.connect(MONGU_URI).then(connection => {
-            const query = {}
-            query[Model.IdField] = parseInt(req.params.id)
-            connection.db().collection(Model.collectionName).findOne(query).then(data => {
-                if (!data) {
-                    res.sendStatus(404)
-                    return
-                }
+  router.get('/:id', (req, res) => {
+    MongoClient.connect(MONGU_URI).then(connection => {
+      connection.db().collection(Model.collectionName).findOne(idMongoQuery(Model.IdField, parseInt(req.params.id))).then(data => {
+        if (!data) {
+          res.sendStatus(404)
+          return
+        }
 
-                res.send(data)
-            })
-        }).catch(err => {
-            MsLogger.error(err)
-            res.sendStatus(500)
-        })
+        res.send(data)
+      })
+    }).catch(err => {
+      MsLogger.error(err)
+      res.sendStatus(500)
     })
+  })
 
-    router.put('/:id', (req, res) => {
-        MongoClient.connect(MONGU_URI).then(connection => {
-            const query = {}
-            query[Model.IdField] = parseInt(req.params.id)
-            // TODO: validate
-            connection.db().collection(Model.collectionName).findOneAndUpdate(query, req.body).then(dbResponse => {
-                if (dbResponse.ok === 1) {
-                    res.sendStatus(200)
-                }
-            })
-        }).catch(err => {
-            MsLogger.error(err)
-            res.sendStatus(500)
-        })
+  router.put('/:id', (req, res) => {
+    MongoClient.connect(MONGU_URI).then(connection => {
+      // TODO: validate
+      connection.db().collection(Model.collectionName).findOneAndUpdate(idMongoQuery(Model.IdField, parseInt(req.params.id)), req.body).then(dbResponse => {
+        if (dbResponse.ok === 1) {
+          res.sendStatus(200)
+        }
+      })
+    }).catch(err => {
+      MsLogger.error(err)
+      res.sendStatus(500)
     })
+  })
 
-    router.post('/', (req, res) => {
-        MongoClient.connect(MONGU_URI).then(connection => {
-            const query = {}
-            query[Model.IdField] = req.body[Model.IdField]
-            connection.db().collection(Model.collectionName).findOne(query).then(data => {
-                if (data) {
-                    res.status(400)
-                    res.send('Object already exists')
-                    return
-                }
-                // TODO: validate
-                connection.db.collection(Model.collectionName).insertOne(req.body).then(a => {
-                    if (a.insertedCount > 0) {
-                        res.sendStatus(200)
-                    }
-                })
-            })
-        }).catch(err => {
-            MsLogger.error(err)
-            res.sendStatus(500)
+  router.post('/', (req, res) => {
+    MongoClient.connect(MONGU_URI).then(connection => {
+      connection.db().collection(Model.collectionName).findOne(idMongoQuery(Model.IdField, req.body[Model.IdField])).then(data => {
+        if (data) {
+          res.status(400)
+          res.send('Object already exists')
+          return
+        }
+        // TODO: validate
+        connection.db.collection(Model.collectionName).insertOne(req.body).then(a => {
+          if (a.insertedCount > 0) {
+            res.sendStatus(200)
+          }
         })
+      })
+    }).catch(err => {
+      MsLogger.error(err)
+      res.sendStatus(500)
     })
+  })
 
-    router.delete('/:id', (req, res) => {
-        MongoClient.connect(MONGU_URI).then(connection => {
-            const query = {}
-            query[Model.IdField] = parseInt(req.params.id)
-            connection.db().collection(Model.collectionName).findOneAndDelete(query).then(dbResponse => {
-                if (dbResponse.ok === 1) {
-                    res.sendStatus(200)
-                }
-            })
-        }).catch(err => {
-            MsLogger.error(err)
-            res.sendStatus(500)
-        })
+  router.delete('/:id', (req, res) => {
+    MongoClient.connect(MONGU_URI).then(connection => {
+      connection.db().collection(Model.collectionName).findOneAndDelete(idMongoQuery(Model.IdField, parseInt(req.params.id))).then(dbResponse => {
+        if (dbResponse.ok === 1) {
+          res.sendStatus(200)
+        }
+      })
+    }).catch(err => {
+      MsLogger.error(err)
+      res.sendStatus(500)
     })
+  })
 
-    return router
+  return router
+}
+
+function idMongoQuery(idField, id) {
+  const query = {}
+  query[idField] = id
+  return query
 }
