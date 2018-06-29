9c1efc0bcb5490dd8ba8f580d0462956ea7d04d0
Tal Taub <taltaub22@gmail.com>
2018-06-29 22:56:47 +0300

    Removed log line

--- routers/matchTeamRouter.js
+++ routers/matchTeamRouter.js
@@ -13,7 +13,6 @@ exports.getRouter = function () {
   const router = express.Router()
 
   router.get('/:team/matches', (req, res) => {
-    MsLogger.debug(req.params.team)
     MongoClient.connect(MONGU_URI).then(connection => {
       connection.db().collection('matches').find({ 'matchTeams.teamNumber': parseInt(req.params.team) }).toArray().then(data => {
         if (!data) {
