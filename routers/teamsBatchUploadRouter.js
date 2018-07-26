7cfc4e10ddeb7c619f641488303650a80c357ebc
Idan Stark <idanstark42@gmail.com>
2018-07-26 22:52:22 +0300

    Added admin authorization

--- routers/teamsBatchUploadRouter.js
+++ routers/teamsBatchUploadRouter.js
@@ -2,23 +2,24 @@
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient
 const MsLogger = require('@first-lego-league/ms-logger').Logger()
+const { authroizationMiddlware } = require('@first-lego-league/ms-auth')
 
 const teamsBatchParser = require('../logic/teamsBatchParser')
 
 const MONGU_URI = process.env.MONGO_URI
 
+const adminAction = authroizationMiddlware(['admin', 'development'])
+
 exports.getRouter = function () {
   const router = express.Router()
 
-  router.post('/batch', (req, res) => {
+  router.post('/batch', adminAction, (req, res) => {
     if (!req.body.teamsData) {
-      res.status(400)
-      res.send('Please provide data..')
+      return res.status(400).send('Please provide data..')
     }
 
     if (!req.body.delimiter) {
-      res.status(400)
-      res.send('Please provide delimiter..')
+      return res.status(400).send('Please provide delimiter..')
     }
 
     MongoClient.connect(MONGU_URI).then(connection => {
