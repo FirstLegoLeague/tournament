74acd86dc58be484cdadf398c481caabe56198f8
Jonathan Roberts Gideoni <yonatan.gideoni@gmail.com>
2018-08-02 23:41:57 +0300

    WIP Router

--- routers/tournamentStatusRouter.js
+++ routers/tournamentStatusRouter.js
@@ -1,12 +1,29 @@
 'use strict'
 const express = require('express')
-
 const MongoClient = require('mongodb').MongoClient
 const MsLogger = require('@first-lego-league/ms-logger').Logger()
-const { authroizationMiddlware } = require('@first-lego-league/ms-auth')
 
 const MONGU_URI = process.env.MONGO_URI
 
 exports.getRouter = function () {
-    const router = express.Router()
+  const router = express.Router()
+
+  router.get('/match/all/:number', (req, res) => {
+    MongoClient.connect(MONGU_URI).then(connection => {
+      connection.db().collection('matches').then(data => {
+        if (!data) {
+          res.sendStatus(404)
+          return
+        }
+        
+        console.log(data)
+      })
+    }).catch(err => {
+      console.log(err)
+      MsLogger.error(err)
+      res.sendStatus(500)
+    })
+  })
+
+  return router
 }
