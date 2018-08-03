3b386ade89af06702f014e5fb6cd9e8cd91dde82
Jonathan Roberts Gideoni <yonatan.gideoni@gmail.com>
2018-08-03 11:54:55 +0300

    Created getMatch function.

--- logic/getMatchLogic.js
+++ logic/getMatchLogic.js
@@ -4,10 +4,27 @@ const MsLogger = require('@first-lego-league/ms-logger').Logger()
 
 const MONGU_URI = process.env.MONGO_URI
 
-let match = 0
+const match = 0
 
 function getCurrentMatch () {
-    MongoClient.connect(MONGU_URI).then(connection => {
-        connection.db().collection('matches').findOne()
-    })
+
+}
+
+const getMatch = function (matchNumber) {
+  MongoClient.connect(MONGU_URI).then(connection => {
+    const level = connection.db().collection('settings').find({})[0].tournamentLevel
+    const dbMatch = connection.db().collection('matches').findOne({ 'matchId': match }, { 'stage': level })
+
+    if (dbMatch) {
+      return {
+        'match': dbMatch.matchId,
+        'startTime': dbMatch.startTime,
+        'endTime': dbMatch.endTime
+      }
+    }
+    return null // happens if match does not exist in DB
+  }).catch(err => {
+    MsLogger.error(err)
+    throw err
+  })
 }
