986cd0304df77ec5eb21cb25976777b859db6220
Jonathan Roberts Gideoni <yonatan.gideoni@gmail.com>
2018-08-03 12:25:29 +0300

    Created getNextMatches function.

--- logic/getMatchLogic.js
+++ logic/getMatchLogic.js
@@ -4,16 +4,33 @@ const MsLogger = require('@first-lego-league/ms-logger').Logger()
 
 const MONGU_URI = process.env.MONGO_URI
 
-const match = 0
+const MATCH = 0
+const UPCOMING_MATCHES_TO_GET = 2
 
 function getCurrentMatch () {
+  return getMatch(MATCH)
+}
 
+function getNextMatches () {
+  const retMatches = []
+
+  for (let i = 1; i <= UPCOMING_MATCHES_TO_GET; i++) {
+    const tempMatch = getMatch(MATCH + i)
+
+    if (tempMatch) {
+      retMatches.push(tempMatch)
+    } else { // stops if final match is reached/match does not exist in DB
+      break
+    }
+  }
+
+  return retMatches
 }
 
 const getMatch = function (matchNumber) {
   MongoClient.connect(MONGU_URI).then(connection => {
     const level = connection.db().collection('settings').find({})[0].tournamentLevel
-    const dbMatch = connection.db().collection('matches').findOne({ 'matchId': match }, { 'stage': level })
+    const dbMatch = connection.db().collection('matches').findOne({ 'matchId': matchNumber }, { 'stage': level })
 
     if (dbMatch) {
       return {
@@ -27,4 +44,9 @@ const getMatch = function (matchNumber) {
     MsLogger.error(err)
     throw err
   })
+}
+
+module.exports = {
+  'getCurrentMatch': getCurrentMatch,
+  'getNextMatches': getNextMatches
 }
