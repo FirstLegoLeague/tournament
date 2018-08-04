75919969265c70e83e8032b25212f0ef91cc895f
Idan Stark <idanstark42@gmail.com>
2018-08-05 00:00:37 +0300

    linting teamLogic

--- logic/teamLogic.js
+++ logic/teamLogic.js
@@ -1,9 +1,11 @@
+'use strict'
+
 const MongoClient = require('mongodb').MongoClient
 const MsLogger = require('@first-lego-league/ms-logger').Logger()
 
 const MONGU_URI = process.env.MONGO_URI
 
-module.exports.deleteValidation = function (params) {
+exports.deleteValidation = function (params) {
   try {
     deleteMatchesForTeam(parseInt(params.id))
     return true
@@ -14,7 +16,7 @@ module.exports.deleteValidation = function (params) {
 
 function deleteMatchesForTeam (teamNumber) {
   MongoClient.connect(MONGU_URI).then(connection => {
-    connection.db().collection('matches').updateOne({ 'matchTeams.teamNumber': teamNumber },
+    return connection.db().collection('matches').updateOne({ 'matchTeams.teamNumber': teamNumber },
       { $set: { 'matchTeams.$.teamNumber': 'null' } },
       {
         'arrayFilters':
@@ -24,11 +26,10 @@ function deleteMatchesForTeam (teamNumber) {
           }]
       }
     )
-      .then(dbResponse => {
-        if (dbResponse.modifiedCount > 0) {
-          MsLogger.info('Matches were updates successfully')
-        }
-      })
+  }).then(dbResponse => {
+    if (dbResponse.modifiedCount > 0) {
+      MsLogger.info('Matches were updates successfully')
+    }
   }).catch(err => {
     MsLogger.error(err)
     throw err
