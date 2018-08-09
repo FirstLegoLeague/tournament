adc9d5f6eb596d19074186b0436952bfdafec0f9
Tal Taub <taltaub22@gmail.com>
2018-08-10 00:03:54 +0300

    Added error logs when returning 500 to the user

--- routers/imagesRouter.js
+++ routers/imagesRouter.js
@@ -1,20 +1,21 @@
 'use strict'
 const express = require('express')
 const {authroizationMiddlware} = require('@first-lego-league/ms-auth')
+const {Logger} = require('@first-lego-league/ms-logger')
 
 const {getAllImages, getImage, saveImageFromBase64, deleteImage, initImagesFolder} = require('../logic/imagesLogic')
 
 const adminAction = authroizationMiddlware(['admin', 'development'])
-
+const logger = Logger()
 const router = express.Router()
 
 initImagesFolder()
-
 
 router.get('/all', (req, res) => {
   getAllImages().then(images => {
     res.send(images)
   }).catch(err => {
+    logger.error(`An error has occurred ${err}`)
     res.status(500)
     res.send(err.message)
   })
@@ -24,6 +25,7 @@ router.get('/:imageName', (req, res) => {
   getImage(req.params.imageName).then(image => {
     res.send(image)
   }).catch(err => {
+    logger.error(`An error has occurred ${err}`)
     res.status(500)
     res.send(err.message)
   })
@@ -33,6 +35,7 @@ router.post('/', adminAction, (req, res) => {
   saveImageFromBase64(req.body.name, req.body.image).then(() => {
     res.sendStatus(201)
   }).catch(e => {
+    logger.error(`An error has occurred ${err}`)
     res.status(500)
     res.send(e.message)
   })
@@ -42,6 +45,7 @@ router.delete('/:imageName', adminAction, (req, res) => {
   deleteImage(req.params.imageName).then(() => {
     res.sendStatus(204)
   }).catch(err => {
+    logger.error(`An error has occurred ${err}`)
     res.status(500)
     res.send(err.message)
   })
