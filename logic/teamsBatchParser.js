782c1eaa203e938133fc71e1d6bf0204fde78f04
Tal Taub <taltaub22@gmail.com>
2018-08-29 20:23:30 +0300

    Checking if a line is empty when parsing team list file

--- logic/teamsBatchParser.js
+++ logic/teamsBatchParser.js
@@ -3,7 +3,7 @@
 const objectDataParser = require('./objectDataParser')
 
 exports.parse = function (data, delimiter) {
-  const teams = data.split('\n')
+  const teams = data.split('\n').filter(line => line.trim() !== '')
     .map(line => objectDataParser.deserializeTeam(line.split(delimiter)))
 
   if (teams.length > 0 && teams[0].number === undefined) {
