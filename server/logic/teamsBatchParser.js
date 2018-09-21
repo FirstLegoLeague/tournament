d5b6e1e7d6dcd50d696eb681222c659862af80bf
Tal Taub <taltaub22@gmail.com>
2018-09-21 22:27:28 +0300

    Moves server files to server fodler

---
+++ server/logic/teamsBatchParser.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const objectDataParser = require('./objectDataParser')
+
+const MsLogger = require('@first-lego-league/ms-logger').Logger()
+
+exports.parse = function (data, delimiter) {
+  let errorStr
+  // Filter out lines without any data
+  const teams = data.split('\n').filter(line => line.trim() !== '')
+    .map(line => objectDataParser.deserializeTeam(line.split(delimiter)))
+
+  if (teams.length > 0 && teams[0].number === undefined) {
+    teams.unshift()
+  }
+  MsLogger.info(`Parsing team list file. Found ${teams.length} team(s)`)
+
+  // Check for duplicate team numbers. Cause import to fail if found
+  const teamNumbersArray = teams.map(team => team.number)
+  const hasDuplicateTeam = teamNumbersArray.some((team, index) => {
+    return teamNumbersArray.indexOf(team) !== index
+  })
+  if (hasDuplicateTeam) {
+    errorStr = 'Duplicate team number found in team list. Aborting import'
+    MsLogger.warn(errorStr)
+    teams.length = 0
+  }
+
+  return { 'teams': teams, 'error': errorStr }
+}
