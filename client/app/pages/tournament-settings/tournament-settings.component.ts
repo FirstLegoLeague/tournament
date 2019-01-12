5ca5d7326bdfd1bb3a4efe24545fe606b6d69901
Tal Taub <taltaub22@gmail.com>
2019-01-12 20:01:26 +0200

    fix(current-match-error-msg): Using error message from server

--- client/app/pages/tournament-settings/tournament-settings.component.ts
+++ client/app/pages/tournament-settings/tournament-settings.component.ts
@@ -117,7 +117,8 @@ export class TournamentSettingsComponent implements OnInit {
         this.tournamentStatusService.setMatch(this.settings['currentMatch'].value).subscribe(() => {
             this.notification.success("Match saved successfully")
         }, (err) => {
-            this.notification.error("Oh no! Something went wrong while trying to save match.")
+            console.log(err)
+            this.notification.error(err.error.error)
         })
     }
 
