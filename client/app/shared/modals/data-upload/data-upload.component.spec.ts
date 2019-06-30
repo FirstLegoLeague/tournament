2fde87b25ae17093d4d432ca69686977751927d7
LPNG1 <itamar.oryan@gmail.com>
2019-06-30 21:49:43 +0300

    Handled promises

--- client/app/shared/modals/data-upload/data-upload.component.spec.ts
+++ client/app/shared/modals/data-upload/data-upload.component.spec.ts
@@ -1,22 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { DataUpload } from './data-upload.component'
-import { LoggerService } from '../../services/logger.service'
-import { ErrorHandler } from '@angular/core'
 
 describe('DataUpload', () => {
   let component: DataUpload
   let fixture: ComponentFixture<DataUpload>
-  let logger: LoggerService
-  let handler: ErrorHandler
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
+    return TestBed.configureTestingModule({
       declarations: [DataUpload]
     })
     .compileComponents()
-    .catch(err => handler.handleError(err))
-    .then(() => logger.error('Could not compile components'))
   }))
 
   beforeEach(() => {
