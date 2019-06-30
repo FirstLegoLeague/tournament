c6f4dd7a2c7695d054de011fde7786de2b28b9d8
LPNG1 <itamar.oryan@gmail.com>
2019-06-30 12:40:32 +0300

    Fixed lint errors

--- client/app/shared/modals/data-upload/data-upload.component.spec.ts
+++ client/app/shared/modals/data-upload/data-upload.component.spec.ts
@@ -1,25 +1,32 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import {DataUpload} from './data-upload.component'
+import { DataUpload } from './data-upload.component'
+import { LoggerService } from '../../services/logger.service'
+import { ErrorHandler } from '@angular/core'
 
 describe('DataUpload', () => {
-    let component: DataUpload;
-    let fixture: ComponentFixture<DataUpload>
+  let component: DataUpload
+  let fixture: ComponentFixture<DataUpload>
+  let logger: LoggerService
+  let handler: ErrorHandler
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            declarations: [ DataUpload ]
-        })
-        .compileComponents()
-    }))
-
-    beforeEach(() => {
-        fixture = TestBed.createComponent(DataUpload)
-        component = fixture.componentInstance
-        fixture.detectChanges()
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [DataUpload]
     })
+    .compileComponents()
+    .catch(err => handler.handleError(err))
+    .then(() => logger.error('Could not compile components'))
+    .catch(() => 'Obligatory catch')
+  }))
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    })
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DataUpload)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should create', () => {
+    expect(component).toBeTruthy()
+  })
 })
